f4fe01548727505d5761328922ed60d015d71eed
Mohit D Patel <mdpatel.csecon@outlook.com>
2026-09-05 18:09:03 -0400

    Split ACPI docs and refactor MADT code

--- crates/catten/src/environment/acpi/mod.rs
+++ crates/catten/src/environment/acpi/mod.rs
@@ -1,36 +1,4 @@
-//! The Advanced Configuration and Power Interface (ACPI) Subsystem
-//!
-//! The Advanced Configuration and Power Interface (ACPI) is an open industry specification that
-//! defines a flexible and extensible interface for hardware discovery, configuration, power
-//! management, and monitoring. ACPI provides a standardized way for the operating system to
-//! interact with the underlying hardware, allowing it to manage power states, configure devices,
-//! and perform other system-level tasks in a platform-independent manner.
-//!
-//! It provides information in two different forms:
-//!
-//! - System Description Tables (SDTs)
-//! - ACPI Machine Language (AML) bytecode tables
-//!
-//! This module is split into two submodules, `sdt` and `aml`, which contain code for working with
-//! each of these forms of information respectively and their inline documentation contains more
-//! detailed information about how each of them works and how Catten uses them.
-//!
-//! This top level module contains code for finding and parsing the XSDT to find the physical
-//! addresses of other ACPI tables, as well as some common data structures and utilities for working
-//! with the headers that are common to all ACPI tables.
-//!
-//! The primary reference needed to understand and work with this module is the [ACPI specification](https://uefi.org/specs/ACPI/6.6/).
-//! In addition to the specification the source code of [uACPI](https://github.com/uACPI/uACPI) a portable C language
-//! ACPI implementation made to be robust enough to handle buggy firmware can be very helpful to
-//! understand how to work with ACPI and handle various edge cases and quirks of real world
-//! firmware.
-//!
-//! It should be noted however that the Catten kernel does not and will not integrate uACPI or any
-//! other third party ACPI implementation. Accordingly this subsystem is to be developed entirely
-//! independently in manually written Rust and in such a way as to be tightly integrated with the
-//! rest of the kernel. Features will be added as they are needed and the implementation will
-//! generally assume that target system firmware appropriately conforms to the latest published ACPI
-//! specification.
+#[doc = include_str!("doc.md")]
 
 pub mod aml;
 pub mod sdt;
---
+++ crates/catten/src/environment/acpi/sdt/madt/index.rs
@@ -0,0 +1,91 @@
+//! # Multiple APIC Description Table (MADT)
+
+use alloc::vec::Vec;
+use core::ptr::NonNull;
+
+use crate::cpu::isa::interface::memory::address::VirtualAddressIfce;
+use crate::environment::acpi::sdt::madt::entry_types::MadtEntryType;
+use crate::environment::acpi::table_map::AcpiTableHeader;
+use crate::memory::VirtualAddress;
+
+
+
+const NUM_ENTRY_TYPES: usize = 28usize;
+
+pub struct MadtEntryIndex {
+    ptr_matrix: [Vec<NonNull<MadtEntryGeneric>>; NUM_ENTRY_TYPES],
+}
+
+impl MadtEntryIndex {
+    fn get_type(&self, entry_type: MadtEntryType) -> &Vec<NonNull<MadtEntryGeneric>> {
+        &self.ptr_matrix[entry_type as usize]
+    }
+}
+
+#[derive(Debug)]
+#[repr(C, packed)]
+struct MadtEntryGeneric {
+    entry_type: u8,
+    entry_length: u8,
+}
+
+struct MadtEntryIter {
+    ptr: Option<NonNull<MadtEntryGeneric>>,
+    end_ptr: VirtualAddress,
+}
+
+impl MadtEntryIter {
+    pub fn new(madt_ptr: *const Madt) -> Self {
+        Self {
+            ptr: unsafe {
+                NonNull::new((madt_ptr as *const u8).add(core::mem::size_of::<Madt>())
+                    as *mut MadtEntryGeneric)
+            },
+            end_ptr: VirtualAddress::from_ptr(unsafe {
+                (madt_ptr as *const u8).add((*madt_ptr).header.length as usize)
+            }),
+        }
+    }
+}
+
+impl Iterator for MadtEntryIter {
+    type Item = NonNull<MadtEntryGeneric>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(nn_ptr) = self.ptr {
+            let entry_length = unsafe { nn_ptr.read() }.entry_length;
+            if VirtualAddress::from_ptr(unsafe { nn_ptr.as_ptr().add(entry_length as usize) })
+                > self.end_ptr
+            {
+                self.ptr = None;
+            } else {
+                self.ptr = NonNull::new(unsafe { nn_ptr.as_ptr().add(entry_length as usize) });
+            }
+        }
+        self.ptr
+    }
+}
+
+#[derive(Debug)]
+#[repr(C, packed)]
+pub struct Madt {
+    header: AcpiTableHeader,
+    lapic_address: u32,
+    flags: u32,
+}
+
+impl Madt {
+    pub fn parse(&self) -> MadtEntryIndex {
+        let mut ptr_matrix: [Vec<NonNull<MadtEntryGeneric>>; NUM_ENTRY_TYPES] = Default::default();
+        let iter = MadtEntryIter::new(self);
+        for entry_ptr in iter {
+            let entry_type = unsafe { entry_ptr.as_ref() }.entry_type as usize;
+            if entry_type < NUM_ENTRY_TYPES {
+                ptr_matrix[entry_type].push(entry_ptr);
+            }
+        }
+        MadtEntryIndex {
+            ptr_matrix,
+        }
+    }
+}
