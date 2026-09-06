2156ec0360e5339784993cd13ada4d91b3c28366
Mohit D Patel <mdpatel.csecon@outlook.com>
2026-09-06 02:31:45 -0400

    Refactor interrupt routing and ACPI handling: rename types, remove legacy IRQ mapping, and improve function names for clarity.

--- crates/catten/src/device_management/interrupt_routing/legacy_irqs.rs
+++
@@ -1,4 +0,0 @@
-pub(super) static IRQ_GSI_MAP: spin::LazyLock<[u32; 16]> = spin::LazyLock::new(|| {
-    let mut mapping = core::array::from_fn(|i| i as u8);
-    todo!("Correct for MADT overrides");
-});
